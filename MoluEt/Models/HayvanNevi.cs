76af108ecc9cbf31b006cbaf43816c9225868b6c
Mahmut KELES <mahmutt.keles38@gmail.com>
2024-07-22 12:37:19 +0300

    Add project files.

---
+++ MoluEt/Models/HayvanNevi.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Principal;
+
+namespace MoluEt.Models
+{
+    public class HayvanNevi
+    {
+        public int SIRKETNO { get; set; }
+        public int NEVI_NO { get; set; }
+        public string? NEVI_ADI { get; set; }
+        public int? BESI_GURUBU { get; set; }
+        public string? ACIKLAMA { get; set; }
+        public string? INP_USER { get; set; }
+        public string? INP_DATE { get; set; }
+        public string? UDP_USER { get; set; }
+        public string? UDP_DATE { get; set; }
+    }
+}
